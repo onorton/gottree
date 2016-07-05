04fe882ff9f2b766ae6a900cdac05240b502ba79
Oliver Norton <onorton125@gmail.com>
2016-07-05 22:37:03 +0100

    Revert "Added controller for houses so which sorts by house and includes women married to people in a house."
    This reverts commit 900cd5ae8b569b0cb993b19884ded3b0705d9e3a.

--- app/controllers/house_controller.rb
+++
@@ -1,6 +0,0 @@
-class HouseController < ApplicationController
-  def index
-    @house = params[:house]
-    @people = Person.where("house = ? OR gender = 'F' AND spouse IN (SELECT id FROM people WHERE house = ?)", @house, @house) 
-  end
-end
