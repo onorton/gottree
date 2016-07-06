5bb8f897ef8e87eeabfc3e6a9f7187ac9d395f21
onorton <onorton125@gmail.com>
2016-07-06 21:26:35 +0000

    Added simple base case for family tree

--- app/controllers/full_tree_controller.rb
+++ app/controllers/full_tree_controller.rb
@@ -1,6 +1,9 @@
 
 class FullTreeController < ApplicationController
   def index
+    @people = Person.all
+    @tree_html = generateHTML(@people)
+=begin 
     @tree_html = "<div class=\"tree\">
 		<ul>
 			<li>
@@ -46,5 +49,16 @@ class FullTreeController < ApplicationController
 			</li>
 		</ul>
 	</div>"
+=end
   end
+
+  def generateHTML(people)
+     return "<div class=\"tree\"><ul>" + generateHTMLPerson(people[0], people) + "</ul></div>"
+  end
+
+  def generateHTMLPerson(person, people)
+     result = "<li><a href=\"#\"><p>" + person.name.to_s + "</p><p>" + person.year_of_birth.to_s + "-" + person.year_of_death.to_s + "</p></a></li>"
+     return result
+  end
+
 end
