ea87d0c8de2ef538ac8b9326f95a1bf215d74b9a
onorton <onorton125@gmail.com>
2016-07-06 20:44:48 +0000

    Added html of family tree to test.

--- app/controllers/full_tree_controller.rb
+++ app/controllers/full_tree_controller.rb
@@ -1,6 +1,50 @@
 
 class FullTreeController < ApplicationController
   def index
-    @tree_html = "<div class=\"tree\">Hello</div>"
+    @tree_html = "<div class=\"tree\">
+		<ul>
+			<li>
+				<a href=\"#\">
+          <p>Eddard Stark</p>
+          <p>263-299</p>
+        </a><a href=\"#\">
+          <p>Catelyn Stark</p>
+          <p>263-299</p>
+        </a>
+				<ul>
+					<li>
+            <a href=\"#\">
+              <p>Robb Stark</p>
+              <p>263-299</p>
+            </a>
+					</li>
+          <li>
+            <a href=\"#\">
+              <p>Sansa Stark</p>
+              <p>263-299</p>
+            </a>
+					</li>
+          <li>
+            <a href=\"#\">
+              <p>Arya Stark</p>
+              <p>263-299</p>
+            </a>
+					</li>
+          <li>
+            <a href=\"#\">
+              <p>Bran Stark</p>
+              <p>263-299</p>
+            </a>
+					</li>
+          <li>
+            <a href=\"#\">
+              <p>Rickon Stark</p>
+              <p>263-299</p>
+            </a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>"
   end
 end
