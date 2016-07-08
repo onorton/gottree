50cbd9205df2224c9c599920f266f2777ec5e469
onorton <onorton125@gmail.com>
2016-07-08 19:12:02 +0000

    Now have tree generated based off of database.

--- app/controllers/full_tree_controller.rb
+++ app/controllers/full_tree_controller.rb
@@ -3,56 +3,35 @@ require 'graphviz'
 class FullTreeController < ApplicationController
   def index
     @people = Person.all
+
+    family_tree = GraphViz.digraph( "family" ) { |family_tree| 
+      family_tree[:ratio] = 'auto'
+      @people.each do |p|
+        family_tree.add_nodes( p.id.to_s, :color => 'black', :label => p.name, :regular => '1', :shape => 'box' )
+      end
+
+      @people.each do |p|
+         if p.spouse != 0 && family_tree.get_node(p.spouse.to_s + "+" + p.id.to_s).nil? 
+           family_tree.add_nodes( p.id.to_s + "+" + p.spouse.to_s, :color => '', :label => '', :regular => '', :shape => 'point' ) 
+           family_tree.add_edges(p.id.to_s, p.id.to_s + "+" + p.spouse.to_s, :dir=>'none')
+           family_tree.add_edges(p.spouse.to_s, p.id.to_s + "+" + p.spouse.to_s , :dir=>'none')
+         end
+      end
+
+      @people.each do |p|
+         if p.father != 0 && p.mother != 0
+            if !family_tree.get_node(p.father.to_s + "+" + p.mother.to_s).nil?
+              family_tree.add_edges(p.father.to_s + "+" + p.mother.to_s, p.id.to_s, :dir=>'none')
+            end
+        
+            if !family_tree.get_node(p.mother.to_s + "+" + p.father.to_s).nil?
+              family_tree.add_edges(p.mother.to_s + "+" + p.father.to_s, p.id.to_s, :dir=>'none')
+            end
+         end 
+      end    
+    }
   
-    GraphViz.parse( "family_tree.dot") {|g|}.output(:svg => "app/assets/images/family_tree.svg") 
-  end
-
-  def generateHTML(people)
-     return "<div class=\"tree\"><ul>" + generateHTMLPerson(people[0], people) + "</ul></div>"
-  end
-
-  def generateHTMLPerson(person, people)
-     result = "<li><a href=\"#\"><p>" + person.name.to_s + "</p><p>" + checkYear(person.year_of_birth) + "-" + checkYear(person.year_of_death) + "</p></a>"
-     spouse = findSpouse(person, people)
-     if spouse != nil
-     	result = result + "<a href=\"#\"><p>" + spouse.name.to_s + "</p><p>" + checkYear(spouse.year_of_birth) + "-" + checkYear(spouse.year_of_death) + "</p></a>"
-     end
-     children = findChildren(person, people)
-     if children.count != 0
-        result = result + "<ul>"
-        children.each do |c|
-          result = result + generateHTMLPerson(c, people)
-        end
-        result = result + "</ul>"
-     end 
-     result = result + "</li>"
-     return result 
-  end
-
-  def checkYear(year)
-     unless year == 0
-        return year.to_s
-     end
-     return ""
-  end
-
-  def findSpouse(person, people)
-    people.each do |p|
-       if person.spouse == p.id
-          return p
-       end
-    end
-    return nil
-  end
-
-  def findChildren(person, people)
-    children = Array.new
-    people.each do |c|
-       if person.id == c.father or person.id == c.mother
-          children.insert(0, c)
-       end
-    end
-    return children.reverse
+    family_tree.output(:svg => "app/assets/images/family_tree.svg") 
   end
 
 
