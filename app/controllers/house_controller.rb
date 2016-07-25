8abb12da215ce9b9ee05e3158ca1854fbecd7199
onorton <onorton125@gmail.com>
2016-07-25 16:09:44 +0000

    Changed rules for houses so that person_1 is the dominant house (children born under legitimate relationship are born under this house).

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -3,7 +3,7 @@ require 'tree_renderer'
 class HouseController < ApplicationController
   def display
     @house = params[:name]
-    people = Person.where("house = ? OR gender = 'F' AND  id IN (SELECT person_2 FROM people join relationships ON people.id = relationships.person_1 WHERE house = ? AND legitimate=1)", @house, @house)
+    people = Person.where("house = ? OR id IN (SELECT person_2 FROM people join relationships ON people.id = relationships.person_1 WHERE house = ? AND legitimate=1)", @house, @house)
     relationships = Relationship.where("legitimate = 1")
     renderTree(@house, people, relationships)
   end
