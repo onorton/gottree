e2694342ee2f4d192a19914d63e00169e6c9aebb
onorton <onorton125@gmail.com>
2017-08-20 22:10:50 +0000

    HTML now parsed and NULL wikilinks ignored.

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
-
+require 'nokogiri'
 require 'net/http'
 lines = IO.readlines('characters.txt')
 lines.each do |character|
 	id = character.split("\t").first
-	wikilink = character.split("\t").last
-	url = URI.encode(('http://awoiaf.westeros.org/index.php/' + wikilink).chomp())
-	#puts url
-	puts Net::HTTP.get(URI.parse(url))
+	wikilink = character.split("\t").last.chomp()
+	if (wikilink != "NULL")
+	url = URI.encode('http://awoiaf.westeros.org/index.php/' + wikilink)
+	puts url
+	html =  Nokogiri::HTML(Net::HTTP.get(URI.parse(url)))
+		puts html
+	end
 end
