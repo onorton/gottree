13c6bd331c33a3f5b0357938e97a3dfde550ec15
onorton <onorton125@gmail.com>
2017-08-31 22:00:42 +0000

    Bastards and illegitimate relationships shown dashed.

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -3,7 +3,7 @@ require 'tree_renderer'
 class HouseController < ApplicationController
   def display
     @house = params[:name]
-    people = Person.where("house = ? OR id IN (SELECT person_2 FROM people join relationships ON people.id = relationships.person_1 WHERE house = ? AND legitimate=1)", @house, @house)
+    people = Person.where("house = ? OR id IN (SELECT person_2 FROM people join relationships ON people.id = relationships.person_1 WHERE house = ? AND legitimate = 1)", @house, @house)
     relationships = Relationship.where("legitimate = 1 AND person_1 IN (?) AND person_2 IN (?) ", people.pluck(:id), people.pluck(:id))
 
 
